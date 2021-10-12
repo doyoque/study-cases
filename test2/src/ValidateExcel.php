c61c3b7498f052ba40ea6ab030d6f7eed359c6e2
doyoque <abiyoga1335@gmail.com>
2021-10-12 23:41:42 +0700

    setup composer json and initial class

---
+++ test2/src/ValidateExcel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Doyoque\Excel;
+
+class ValidateExcel {
+
+    /**
+     * @return bool
+     */
+    public function shouldBeWork($bool = true)
+    {
+        return $bool;
+    }
+}
